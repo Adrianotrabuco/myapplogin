8774c4ba7d143d7e87b08b96414872eda5b8f0c9
Adriano Trabuco <Adriano@atcards.com.br>
2026-02-26 09:23:23 -0300

    Atualiza validação de senha em index.tsx

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,95 +1,61 @@
-import { Button } from "@/components/Button";
-import { Input } from "@/components/Input";
-
-import { Link } from "expo-router";
 import { useState } from "react";
-import {
-    Alert,
-    Image,
-    KeyboardAvoidingView,
-    Platform,
-    ScrollView, StyleSheet, Text, View
-} from "react-native";
+import { Alert, Button, StyleSheet, Text, TextInput, View } from "react-native";
 
-export default function Index(){
-    const [email, setEmail] = useState("");
-    const [senha, setSenha] = useState("");
+export default function Index() {
+  const [senha, setSenha] = useState("");
+  const [confirmaSenha, setConfirmaSenha] = useState("");
 
+  function handleCadastrar() {
+    console.log("Botão clicado");
 
-    function handleSignIn(){
-        console.log(email)
-        Alert.alert("Entrar", "Preencha e-mail e senha para entrar!")
+    if (senha !== confirmaSenha) {
+      Alert.alert("Atenção", "Senha não confere!");
+    } else {
+      Alert.alert("Sucesso", "Senha confirmada!");
     }
+  }
 
-    return(
-        <KeyboardAvoidingView 
-            style={{flex:1}}
-            behavior={Platform.select({ios:"padding", android:"height"})}
-        >
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Cadastro</Text>
 
-        <ScrollView 
-            contentContainerStyle={{ flexGrow:1 }}
-            showsVerticalScrollIndicator={false}
-            keyboardShouldPersistTaps="handled"
-        >
-            <View style={styles.container}>
-                <Image 
-                    source={require('@/assets/image1.png')}
-                    style={styles.ilustration} 
-                />
-                <Text style={styles.title}>Entrar {email}</Text>
-                <Text style={styles.subtitle}>Acesse sua conta com e-mail e senha</Text>
-                <View style={styles.form}>
-                    <Input placeholder="E-mail" 
-                        keyboardType="email-address" 
-                        // onChangeText={(text) => console.log(text)}
-                        onChangeText={setEmail}
-                    />
-                    <Input placeholder="Senha" secureTextEntry/>
-                    <Button label="Entrar" onPress={handleSignIn} />
-                    {/* <Button label="Entrar" style={{ backgroundColor: "green"}}/> */}
-                </View>
-                <Text style={styles.footerText}>Não tem uma conta? 
-                    <Link href="/signup" style={styles.footerLink}>
-                        {" "}Cadastre-se aqui
-                    </Link>
-                </Text>
-            </View>
-        </ScrollView>
-        </KeyboardAvoidingView>
-    )
-} 
+      <TextInput
+        placeholder="Senha"
+        secureTextEntry
+        style={styles.input}
+        value={senha}
+        onChangeText={setSenha}
+      />
+
+      <TextInput
+        placeholder="Confirmar Senha"
+        secureTextEntry
+        style={styles.input}
+        value={confirmaSenha}
+        onChangeText={setConfirmaSenha}
+      />
+
+      <Button title="Cadastrar" onPress={handleCadastrar} />
+    </View>
+  );
+}
 
 const styles = StyleSheet.create({
-    container: {
-        flex:1,
-        backgroundColor: "#FDFDFD",
-        padding:32
-    },
-    ilustration:{
-        width: "100%",
-        height: 330,
-        resizeMode:"contain",
-        marginTop:62
-    }, 
-    footerText:{
-        textAlign:"center",
-        marginTop:24,
-        color:"#585860",
-    },
-    footerLink:{
-        color:"#0929b8",
-        fontWeight:700
-    },
-    form: {
-        marginTop:24,
-        gap:12
-    },
-    title:{ 
-        fontSize: 32,
-        fontWeight:900,
-    },
-    subtitle:{
-        fontSize:16,
-    },    
-})
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    padding: 20,
+  },
+  title: {
+    fontSize: 24,
+    marginBottom: 20,
+    textAlign: "center",
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: "#ccc",
+    padding: 10,
+    marginBottom: 15,
+    borderRadius: 5,
+  },
+});
